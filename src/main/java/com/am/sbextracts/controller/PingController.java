8b79278c5ac29f22be792ba3c043c66196ffa92c
alexander.mamedov <alexander_mamedov_cw@bmc.com>
2020-07-29 10:21:52 +0300

    polishing

--- src/main/java/com/am/sbextracts/controller/PingController.java
+++ src/main/java/com/am/sbextracts/controller/PingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/ping")
 public class PingController {
 
-    @PostMapping("challenge")
+    @PostMapping()
     public String urlVerification(@RequestBody Map<String, String> param) {
         return param.get("challenge");
     }
