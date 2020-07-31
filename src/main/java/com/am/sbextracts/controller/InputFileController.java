89c05779bb9710d3bae7d4c8c98489ae1fa437a4
alexm <letter_can_walk@yahoo.com>
2020-07-31 22:06:51 +0300

    adding verification for requests

--- src/main/java/com/am/sbextracts/controller/InputFileController.java
+++ src/main/java/com/am/sbextracts/controller/InputFileController.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +21,9 @@ public class InputFileController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(InputFileController.class);
 
+    @Value("${slack.verification.token}")
+    private String verificationToken;
+
     private final Processor processor;
 
     @Autowired
@@ -27,7 +32,11 @@ public class InputFileController {
     }
 
     @PostMapping
-    public SlackResponse handleFile(MultipartFile file) throws IOException {
+    public SlackResponse handleFile(@RequestHeader("slack-token") String token,
+                                    MultipartFile file) throws IOException {
+        if (!verificationToken.equals(token)) {
+            throw new IllegalArgumentException();
+        }
         if (file == null) {
             LOGGER.info("file is null");
             return new SlackResponse("file is null");
