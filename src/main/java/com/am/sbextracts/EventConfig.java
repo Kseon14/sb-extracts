35f8d4e5ffdd6520962e226ac12d282f03cec491
Alex M <Letter_can_walk@yahoo.com>
2020-08-11 12:58:42 +0300

    minor changes

--- src/main/java/com/am/sbextracts/EventConfig.java
+++ src/main/java/com/am/sbextracts/EventConfig.java
@@ -1,5 +1,6 @@
 package com.am.sbextracts;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
@@ -11,12 +12,13 @@ import java.util.concurrent.Executors;
 @Configuration
 public class EventConfig {
 
-        @Bean(name = "applicationEventMulticaster")
-        public ApplicationEventMulticaster applicationEventMulticaster() {
-            SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
-            ConcurrentTaskExecutor concurrentTaskExecutor = new ConcurrentTaskExecutor();
-            concurrentTaskExecutor.setConcurrentExecutor(Executors.newFixedThreadPool(5));
-            eventMulticaster.setTaskExecutor(concurrentTaskExecutor);
-            return eventMulticaster;
+    @Bean(name = "applicationEventMulticaster")
+    public ApplicationEventMulticaster applicationEventMulticaster(
+            @Value("${event.concurrent.threads}") Integer threadsCount) {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        ConcurrentTaskExecutor concurrentTaskExecutor = new ConcurrentTaskExecutor();
+        concurrentTaskExecutor.setConcurrentExecutor(Executors.newFixedThreadPool(threadsCount));
+        eventMulticaster.setTaskExecutor(concurrentTaskExecutor);
+        return eventMulticaster;
     }
 }
--- src/main/java/com/am/sbextracts/pool/HttpClientPool.java
+++ src/main/java/com/am/sbextracts/pool/HttpClientPool.java
@@ -5,20 +5,22 @@ import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.asynchttpclient.AsyncHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HttpClientPool extends GenericObjectPool<AsyncHttpClient> {
 
     @Autowired
-    public HttpClientPool(PooledObjectFactory<AsyncHttpClient> factory) {
+    public HttpClientPool(PooledObjectFactory<AsyncHttpClient> factory,
+                          @Value("${pool.max-total}")Integer maxTotal,
+                          @Value("${pool.max-idle}") Integer maxIdle) {
         super(factory);
         GenericObjectPoolConfig<AsyncHttpClient> poolConfig = new GenericObjectPoolConfig<>();
-        poolConfig.setMaxIdle(3);
+        poolConfig.setMaxIdle(maxIdle);
         poolConfig.setJmxEnabled(true);
         poolConfig.setJmxNamePrefix("http-client-pool");
-        poolConfig.setMaxTotal(5);
-        poolConfig.setMinIdle(0);
+        poolConfig.setMaxTotal(maxTotal);
         this.setConfig(poolConfig);
     }
 
