7c9b7eadb66bf967a0e3b8c5c451248cc5e574c0
Alex M <Letter_can_walk@yahoo.com>
2020-08-15 22:14:40 +0300

    cache implementation

--- src/main/java/com/am/sbextracts/SbExtractsApplication.java
+++ src/main/java/com/am/sbextracts/SbExtractsApplication.java
@@ -2,11 +2,13 @@ package com.am.sbextracts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+@EnableCaching
 public class SbExtractsApplication {
 
 	public static void main(String[] args) {
