688f774b53075a2eae1253bfeb9decf4887562cc
Alex Mamedov <letter_can_walk@yahoo.com>
2022-02-23 16:45:10 +0200

    increase limits

--- src/main/java/com/am/sbextracts/listener/GlobalVariables.java
+++ src/main/java/com/am/sbextracts/listener/GlobalVariables.java
@@ -1,5 +1,8 @@
 package com.am.sbextracts.listener;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public final class GlobalVariables {
     public static final int DEFAULT_DELAY = 3;
 }
